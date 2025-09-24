a92f5d44d1ada123fd81a1ceed81b09d91419ebf
Abinash Sethi <sethiabinash123@gmail.com>
2025-09-24 04:39:39 -0400

    corrected eso error

--- modules/eso/main.tf
+++ modules/eso/main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 3.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.38"
+    }
+  }
+}
+
 resource "helm_release" "eso" {  
   provider = var.kubernetes_provider
 
